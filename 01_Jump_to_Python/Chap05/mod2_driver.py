5a4a88b67e19b8b28c2eb563c0e3f87c7848b4da
Administrator <Administrator@test.com>
2018-05-11 16:58:53 +0900

    초기 커밋 & Push

---
+++ 01_Jump_to_Python/Chap05/mod2_driver.py
@@ -0,0 +1,13 @@
+import mod2
+
+print(mod2.PI)
+a = mod2.Math()
+print(a.solv(2))
+print(mod2.sum(mod2.PI, 4.4))
+
+
+# if __name__ == "__main__":
+#     print(PI)
+#     a = Math()
+#     print(a.solv(2))
+#     print(sum(PI,4.4))
---
+++ 01_Jump_to_Python/Chap05/mod3_driver.py
@@ -0,0 +1,8 @@
+import sys
+# sys.path.append('E:\\Python_Workspace\\01_Jump_to_Python\\Chap05\\my_util')
+# sys.path.append('E:/Python_Workspace/01_Jump_to_Python/Chap05/my_util')
+sys.path.append('./my_util')
+import mod3
+
+print(mod3.sum(1,2))
+
